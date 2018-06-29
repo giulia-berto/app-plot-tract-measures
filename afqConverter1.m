b7defc33fa0c3dfea74cba7c4d5ecaa458462676
Giulia Bertò <gberto@fbk.eu>
2018-06-29 12:39:16 +0200

    update

--- afqConverter1.m
+++ afqConverter1.m
@@ -7,7 +7,7 @@ if ~isdeployed
 end
 
 config = loadjson('config.json');
-ref_src = fullfile(config.t1_moving);
+ref_src = fullfile(config.t1);
 
 %convert afq to trk
 disp('Converting afq to .trk');
@@ -16,16 +16,7 @@ disp('Converting afq to .trk');
 load(fullfile(config.segmentation));
 fid=fopen('tract_name_list.txt', 'w');
 
-if (config.tract1 > 0)
-    for tract = [config.tract1, config.tract2, config.tract3, config.tract4, config.tract5, config.tract6, config.tract7, config.tract8]
-        if (tract > 0)
-            tract_name=strrep(fg_classified(tract).name,' ','_');
-            write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
-            fprintf(fid, [tract_name, '\n']);
-        end    
-    end  
-  
-elseif length(fg_classified) == 20
+if length(fg_classified) == 20
     disp('AFQ segmentation selected. The following tracts will be returned:')
     disp('Left and Right Thalamic Radiation, Left and Rigth Corticospinal, Left and Right IFOF, Left and Right Arcuate.')
     for tract = [1, 2, 3, 4, 11, 12, 19, 20]
